c22accfe70c6c533d4ad11a3fb07624c96659403
haneen abu zaid <haneensy240@gamail.com>
2024-03-13 00:22:24 +0300

    first ct

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header1 from './components/Header/Header1';
+import Home1 from './Components/Home/Home1';
+import Slaeder from './Components/Slaeder/Slaeder';
+
+import Cart1 from './Components/Crat/Cart1';
+import Footer1 from './Components/Footer/footer1';
+
+
+import Deatils1 from './Components/Deatils/Deatils1';
+import Mycart1 from './Components/MyCart/Mycart1';
+
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+   
+ <Header1 />
+ <Home1 />
+ 
+   <Slaeder  /> 
+   <Cart1 />
+
+<Footer1 />
+<Mycart1 />
+<Deatils1 />
+    </>
+  )
+}
+
+export default App
